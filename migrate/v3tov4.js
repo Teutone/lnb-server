8c796f6dfb63bd3657bf121c0b8d77619567f88c
Benjamin Bädorf <hello@benjaminbaedorf.com>
2017-11-19 21:04:18 +0100

    Ayy

---
+++ migrate/v3tov4.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const inputLocation = process.argv[2];
+const outputLocation = process.argv[3];
+
+const input = require(path.join(process.cwd(), inputLocation));
+
+const output = input.songs.map(({ title, artist, album, episode, id, url }) => ({
+    title,
+    artist,
+    release: album,
+    episode,
+    id,
+    url
+}))
+fs.writeFileSync(path.join(process.cwd(), outputLocation), JSON.stringify(output, null, 4), 'utf8');
