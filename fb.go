7e8c287b9bb4f258a610280d9f70cbab38e8c58d
Benjamin Bädorf <baedorf@convidera.com>
2017-11-24 16:57:09 +0100

    a

--- fb.go
+++ fb.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	fb "github.com/huandu/facebook"
 )
 
@@ -8,5 +10,21 @@ var FbApp *fb.App
 
 func initFb() {
 	FbApp = fb.New(Config.FbAppId, Config.FbAppSecret)
-	FbApp.RedirectUri = "https://" + Config.Sites[0].Hostname + "/"
+	FbApp.RedirectUri = "https://" + Config.Sites[0].Hostname + "/admin/fb-connect"
+}
+
+func getFbLoginRedirect() (string, error) {
+	state, err := GenerateRandomString(12)
+
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf(
+		"https://www.facebook.com/v2.11/dialog/oauth?client_id=%s&redirect_uri=%s&state=%s&scope=%s",
+		Config.FbAppId,
+		FbApp.RedirectUri,
+		state,
+		"manage_pages,publish_pages",
+	), nil
 }
